3b02adfa06e303941aa2c7be1e7d79eabf3228ad
Adebayo Adeniyan <adebeslick@gmail.com>
2019-06-08 23:38:49 +0100

    object creation tutorial complete

--- object-creation/script.js
+++ object-creation/script.js
@@ -1,4 +1,5 @@
 
+//Object creation by function contructor
 var Person =  function(name,yearOfBirth,job){
 
     this.name = name;
@@ -9,7 +10,7 @@ var Person =  function(name,yearOfBirth,job){
 
 Person.prototype.calculateAge = function(){
 
-    console.log(2018-this.yearOfBirth);
+    console.log(this.name+'s age is '+(2018-this.yearOfBirth));
 };
 
 var john = new Person('John',1985,'Teacher');
@@ -17,4 +18,54 @@ var mary = new Person('Mary',1997,'Teacher');
 var jane = new Person('Jane',1998,'Teacher');
 
 
-console.log(john);
+john.calculateAge();
+
+//Object creation by Object.create
+
+var personProto = {
+
+    calculateAge: function(){
+
+        console.log(this.name+'s age is '+(2018 - this.Dob ));
+    }
+}
+
+var mark = Object.create(personProto);
+mark.name = 'Mark';
+mark.Dob = 1988;
+mark.job = 'Teacher';
+
+
+var mary = Object.create(personProto,{
+
+name:{value: 'Mary'},
+Dob:{value: 1978},
+job:{value:'designer'}
+
+});
+mark.calculateAge();
+mary.calculateAge();
+
+//primitives and objects
+
+//primitives
+
+var a = 46;
+var b = a;
+a = 23;
+
+console.log(a,b);
+
+var obj1 = {
+
+    name: 'john',
+    age: 46
+
+};
+
+var obj2 = obj1;
+
+obj1.age = 30;
+
+console.log(obj1.age);
+console.log(obj2.age);
